863f95a953f9873476b1ab6a1385525fa2a93626
ChoiJunYong <thomas19204@gmail.com>
2026-08-12 18:34:09 +0900

    update whole in directory

--- build/Sales_data/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thoma/C++programming/Sales_data
-# Build directory: /home/thoma/C++programming/build/Sales_data
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/StrBlob/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thoma/C++programming/StrBlob
-# Build directory: /home/thoma/C++programming/build/StrBlob
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

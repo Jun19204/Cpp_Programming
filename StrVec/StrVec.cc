f5683b233e772a952ab001384512a4f95482f16f
ChoiJunYong <thomas19204@gmail.com>
2026-08-20 10:24:10 +0900

    StrVec Library Upgrade

--- StrVec/StrVec.cc
+++ StrVec/StrVec.cc
@@ -1,23 +1,31 @@
 #include "StrVec.h"
 #include <algorithm>
-#include <cstddef>
 #include <iterator>
-#include <memory>
-#include <utility>
 
 using namespace std;
 
 void StrVec::push_back(const string &s)
 {
     chk_n_alloc();
-    allocator_traits<decltype(alloc)>::construct(alloc, first_free++, s);
+    allocator_traits<decltype(alloc)>
+        ::construct(alloc, first_free++, s);
+}
+
+void StrVec::push_back(string &&s)
+{
+    chk_n_alloc();
+    allocator_traits<decltype(alloc)>
+        ::construct(alloc, first_free++, std::move(s));
 }
 
 pair<string*, string*>
 StrVec::alloc_n_copy(const string *b, const string *e)
 {
+    if (b == e) {
+        return make_pair(nullptr, nullptr);
+    }
     auto data = alloc.allocate(static_cast<size_t>(e-b));
-    return make_pair(data, uninitialized_copy(b, e, data));
+    return make_pair(data, std::uninitialized_copy(b, e, data));
 }
 
 void StrVec::free()
@@ -26,20 +34,29 @@ void StrVec::free()
         for_each(
                 make_reverse_iterator(first_free), 
                 make_reverse_iterator(elements), 
-                [](const string &s) {
-                    allocator_traits<decltype(alloc)>::destroy(alloc, addressof(s));
+                [](string &s) {
+                    allocator_traits<decltype(alloc)>
+                        ::destroy(alloc, addressof(s));
                 }
         ); 
-        alloc.deallocate(elements, static_cast<size_t>(cap - elements));
+        alloc.deallocate(elements,capacity());
+        elements = first_free = cap = nullptr;
     }
 }
 
 // 복사 제어 - 복사 생성자
 StrVec::StrVec(const StrVec &s)
 {
-    auto newdata = alloc_n_copy(s.begin(), s.end());
-    elements = newdata.first;
-    first_free = cap = newdata.second;
+    auto newpair = alloc_n_copy(s.begin(), s.end());
+    elements = newpair.first;
+    first_free = cap = newpair.second;
+}
+
+// 복사 제어 - 이동 생성자
+StrVec::StrVec(StrVec &&s) noexcept
+    : elements(s.elements), first_free(s.first_free), cap(s.cap)
+{
+    s.elements = s.first_free = s.cap = nullptr;
 }
 
 // 복사 제어 - 복사 대입 연산자
@@ -52,6 +69,19 @@ StrVec& StrVec::operator=(const StrVec &rhs)
     return *this;
 }
 
+// 복사 제어 - 이동 대입 연산자
+StrVec& StrVec::operator=(StrVec &&rhs) noexcept
+{
+    if (this != &rhs) {
+        free();
+        elements = rhs.elements;
+        first_free = rhs.first_free;
+        cap = rhs.cap;
+        rhs.elements = rhs.first_free = rhs.cap = nullptr;
+    }
+    return *this;
+}
+
 // 복사 제어 - 소멸자
 StrVec::~StrVec()
 {
@@ -61,32 +91,15 @@ StrVec::~StrVec()
 void StrVec::reallocate()
 {
     auto newcapacity = size() ? 2 * size() : 1;
-    auto newdata = alloc.allocate(newcapacity);
-    
-    auto dest = newdata;
-    auto elem = elements;
-    
-    for (size_t i = 0; i != size(); ++i) {
-        allocator_traits<decltype(alloc)>::construct(alloc, dest++, std::move(*elem++));
-    }
-    free();
+    auto first = alloc.allocate(newcapacity);
+    auto last = uninitialized_copy(make_move_iterator(begin()), 
+                                   make_move_iterator(end()), 
+                                   first);
 
-    elements = newdata;
-    first_free = dest;
+    free();
+    elements = first;
+    first_free = last;
     cap = elements + newcapacity;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- StrVec/StrVec.h
+++ StrVec/StrVec.h
@@ -7,17 +7,20 @@
 
 class StrVec {
 public:
-    StrVec():
-        elements(nullptr), first_free(nullptr), cap(nullptr) {}
-    StrVec(const StrVec&);
-    StrVec& operator=(const StrVec&);
-    ~StrVec();
+    StrVec() = default; // 기본 생성자
+    StrVec(const StrVec&); // 복사 생성자
+    StrVec(StrVec&&) noexcept; // 이동 생성자
+    StrVec& operator=(const StrVec&) ; // 복사 대입 연산자
+    StrVec& operator=(StrVec&&) noexcept; // 이동 대입 연산자
+    ~StrVec(); // 소멸자
 
     void push_back(const std::string&);
-    size_t size() const 
-        { return static_cast<size_t>(first_free - elements); }
-    size_t capacity() const 
-        { return static_cast<size_t>(cap - elements); }
+    void push_back(std::string&&);
+
+    std::size_t size() const 
+        { return static_cast<std::size_t>(first_free - elements); }
+    std::size_t capacity() const 
+        { return static_cast<std::size_t>(cap - elements); }
     std::string* begin() const { return elements; }
     std::string* end() const { return first_free; }
 
@@ -31,7 +34,7 @@ private:
     void free();
     void reallocate();
 
-    std::string *elements;
-    std::string *first_free;
-    std::string *cap;
+    std::string *elements{nullptr};
+    std::string *first_free{nullptr};
+    std::string *cap{nullptr};
 };

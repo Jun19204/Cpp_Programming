0db507323376c49ac48786e83fc609074f3c546a
Jun19204 <thomas19204@gmail.com>
2026-08-21 14:44:24 +0900

    Sales_data 업그레이드

--- Sales_data/Sales_data.cc
+++ Sales_data/Sales_data.cc
@@ -1,81 +1,62 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
+#include "Sales_data.h"
 
-class Sales_data { 
-friend Sales_data add(const Sales_data&, const Sales_data&);
-friend std::ostream &print(std::ostream&, const Sales_data&);
-friend std::istream &read(std::istream&, Sales_data&);
-
-public:
-    // 생성자
-    Sales_data() = default;
-    Sales_data(const std::string& s) : bookNo(s) {}
-    Sales_data(const std::string& s, unsigned n, double p) :
-               bookNo(s), units_sold(n), revenue(p*n) {}
-    Sales_data(std::istream& is) { 
-        double price = 0;
-        is >> bookNo >> units_sold >> price;
-        revenue = price * units_sold; 
-    }
-
-    // 멤버 함수
-    std::string isbn() const { return bookNo; }
-    Sales_data& combine(const Sales_data&);
-    double avg_price() const;
-
-private:
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
-};
-
-// 프랜드 함수 선언
-Sales_data add(const Sales_data&, const Sales_data&);
-std::ostream &print(std::ostream&, const Sales_data&);
-std::istream &read(std::istream&, Sales_data&);
-
+using namespace std;
 
 // 멤버 함수 정의
-
-inline
-Sales_data& Sales_data::combine(const Sales_data &rhs)
+inline Sales_data &Sales_data::combine(const Sales_data &rhs) 
 {
-    units_sold += rhs.units_sold;
-    revenue += rhs.revenue;
-    return *this;
+  units_sold += rhs.units_sold;
+  revenue += rhs.revenue;
+  return *this;
 }
 
-inline
-double Sales_data::avg_price() const
+inline double Sales_data::avg_price() const 
 {
-    if (units_sold) return revenue/units_sold;
-    else return 0.0;
+  if (units_sold)
+    return revenue / units_sold;
+  else
+    return 0.0;
 }
 
 // 프랜드 함수 정의
 
-Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) 
 {
-    Sales_data sum = lhs;
-    sum.combine(rhs);
-    return sum;
+  Sales_data sum = lhs;
+  sum.combine(rhs);
+  return sum;
 }
 
-std::ostream &print(std::ostream &os, const Sales_data &item)
+ostream &print(ostream &os, const Sales_data &item) 
 {
-    os << item.isbn() << " " << item.units_sold << " "
-       << item.revenue << " " << item.avg_price();
-    return os;
+  os << item.isbn() << " " << item.units_sold 
+     << " " << item.revenue << " " << item.avg_price();
+  return os;
 }
 
-std::istream &read(std::istream &is, Sales_data &item)
-{
-    double price = 0;
-    is >> item.bookNo >> item.units_sold >> price;
-    item.revenue = price * item.units_sold;
-    return is;
+istream &read(istream &is, Sales_data &item) {
+  double price = 0;
+  is >> item.bookNo >> item.units_sold >> price;
+  item.revenue = price * item.units_sold;
+  return is;
 }
+
+istream &operator>>(istream &is, Sales_data &rhs) 
+{
+  double price;
+  is >> rhs.bookNo >> rhs.units_sold >> price;
+  if (is) {
+    rhs.revenue = rhs.units_sold * price;
+  }
+  else {
+    rhs = Sales_data();
+  }
+  return is;
+}
+
+
+
+
 
 
 
---
+++ Sales_data/Sales_data.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <istream>
+#include <string>
+
+class Sales_data {
+  friend Sales_data add(const Sales_data &, const Sales_data &);
+  friend std::ostream &print(std::ostream &, const Sales_data &);
+  friend std::istream &read(std::istream &, Sales_data &);
+  friend std::istream &operator>>(std::istream &, Sales_data &);
+
+public:
+  // 생성자
+  Sales_data() = default;
+  Sales_data(const std::string &s) : bookNo(s) {}
+  Sales_data(const std::string &s, unsigned n, double p)
+      : bookNo(s), units_sold(n), revenue(p * n) {}
+  Sales_data(std::istream &is) {
+    double price = 0;
+    is >> bookNo >> units_sold >> price;
+    revenue = price * units_sold;
+  }
+
+  // 멤버 함수
+  std::string isbn() const { return bookNo; }
+  Sales_data &combine(const Sales_data &);
+  double avg_price() const;
+
+private:
+  std::string bookNo;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
+};
+
+// 프랜드 함수 선언
+Sales_data add(const Sales_data &, const Sales_data &);
+std::ostream &print(std::ostream &, const Sales_data &);
+std::istream &read(std::istream &, Sales_data &);
+std::istream &operator>>(std::istream &, Sales_data &);

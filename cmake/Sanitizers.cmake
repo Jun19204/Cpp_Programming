ed3092bde02df3e97ebd6cb397464b4da4964a41
Jun19204 <thomas19204@gmail.com>
2026-09-01 15:57:46 +0900

    Sanitizers.cmake MSVC분기 추가

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,26 +1,31 @@
 function(enable_sanitizers target)
-  
   if(NOT USE_SANITIZER)
     return()
   endif()
 
-  message(
-    STATUS
-    "ASan + UBSan enable"
-  )
+  if(MSVC)
+    message(STATUS "Enable MSVC AddressSanitizer (ASan)")
 
-  target_compile_options(
-    ${target}
-    INTERFACE
-    -fsanitize=address,undefined
-    -fno-omit-frame-pointer
-  )
+    # MSVC는 /fsanitize=address 플래그 사용 (UBSan 미지원)
+    # Interface 대신 PRIVATE 또는 PUBLIC 권장
+    target_compile_options(${target} INTERFACE /fsanitize=address)
 
-  target_link_options(
-    ${target}
-    INTERFACE
-    -fsanitize=address,undefined
-  )
+    # MSVC ASan 사용 시 증기 연산 라이브러리 지원 플래그
+    target_compile_options(${target} INTERFACE /EHsc)
 
+  elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+    message(STATUS "Enable GCC/Clang ASan + UBSan")
+
+    target_compile_options(
+      ${target}
+      INTERFACE
+      -fsanitize=address,undefined
+      -fno-omit-frame-pointer
+      )
+    target_link_options(
+      ${target}
+      INTERFACE
+      -fsanitize=address,undefined
+      )
+  endif()
 endfunction()
-
